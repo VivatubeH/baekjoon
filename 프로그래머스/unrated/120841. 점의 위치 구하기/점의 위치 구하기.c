a4b743bcf90ce6f344deda88a20311fdf1971963
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-10-30 23:09:17 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// dot_len은 배열 dot의 길이입니다.
+int solution(int dot[], size_t dot_len) {
+        int answer = 0;
+    if ( dot[0] > 0 && dot[1] > 0 )
+    {
+        answer = 1;
+    }
+    if ( dot[0] > 0 && dot[1] < 0 )
+    {
+        answer = 4;
+    }
+    if ( dot[0] < 0 && dot[1] > 0 )
+    {
+        answer = 2;
+    }if ( dot[0] < 0 && dot[1] < 0 )
+    {
+        answer = 3;
+    }
+    return answer;
+}
