87446a049d35328381135b06f5ce9eaa1f50956e
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-11-18 14:14:33 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char name[11];
+	int age;
+	int weight;
+	while (1)
+	{
+		scanf("%s%d%d", name, &age, &weight);
+		if ((name[0] == '#') && (age == 0) && (weight == 0)) break;
+
+		if ((age > 17) || (weight >= 80))
+		{
+			printf("%s Senior\n", name);
+		}
+		else
+		{
+			printf("%s Junior\n", name);
+		}
+		fgetc(stdin);
+	}
+	return 0;
+}
