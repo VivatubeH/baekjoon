bc43d1295ff55dad2dc008ae30ffb72537636829
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-11-20 18:11:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 220 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+	public static void main(String[] args) {
+		
+		long a, b, c;
+		Scanner in = new Scanner(System.in);
+		
+		a = in.nextLong();
+		b= in.nextLong();
+		c=in.nextLong();
+		
+		System.out.println(a+b+c);
+		in.close();
+	}
+}
