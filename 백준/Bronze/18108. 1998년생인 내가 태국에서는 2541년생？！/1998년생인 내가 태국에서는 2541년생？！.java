05aa959eba54e8bbe222974b41a539669a920287
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-11-20 17:34:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int y = in.nextInt();
+		in.close();
+		System.out.println(y -(2541-1998));
+	}
+}
