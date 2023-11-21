bd49a142bf9404ea071a0347b4584b6d21939b7a
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-11-21 22:07:13 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "|\\_/|" << std::endl << "|q p|   /}" << std::endl 
+	<< "( 0 )\"\"\"\\" << std::endl << "|\"^\"`    |" << std::endl << "||_/=\\\\__|" << std::endl;
+	return 0;
+}
