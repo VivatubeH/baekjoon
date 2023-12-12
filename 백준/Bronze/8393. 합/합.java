de0f279921a37ada3a4d02c5cb3159d914caf495
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-12-13 00:43:41 +0900

    [Bronze V] Title: 합, Time: 216 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	
+		int t = in.nextInt();
+		
+			System.out.println(t*(t+1)/2);
+	}
+}
