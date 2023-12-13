18ca842ae7f04db2845039c1bf3fa694aee5589d
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-12-13 23:30:05 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = in.nextInt();
+		int d = in.nextInt();
+		
+		System.out.print(a*b+c*d);
+	}
+}
