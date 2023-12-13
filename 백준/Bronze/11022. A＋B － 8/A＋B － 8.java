915613b6f10d926f76dc8fb8e95828c8493e9fae
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2023-12-13 23:39:28 +0900

    [Bronze V] Title: A+B - 8, Time: 280 ms, Memory: 18988 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int t = in.nextInt();
+		for(int i =1; i<=t;i++) {
+		   int a = in.nextInt();	
+		   int b = in.nextInt();
+		   System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+		}
+	}
+}
