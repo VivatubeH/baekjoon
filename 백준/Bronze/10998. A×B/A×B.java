ec05b3403551e2d375a099574e1b6305c5240cc0
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-29 14:07:37 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
-class Main {
+
+public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in); 
+		// Scanner 클래스의 객체 생성을 먼저 해야함. ( 사용자 입력 )
+		// 참조변수 이름은 자유롭게.
 		
-		int A = in.nextInt();
-		int B = in.nextInt();
-		System.out.println(A*B);
-		in.close();
+		// Scanner 클래스의 nextInt 메서드 활용하기.
+		
+		int a = in.nextInt(); // nextInt는 정수 입력받기.
+		int b = in.nextInt(); // nextInt는 정수 입력받기.
+		
+		System.out.println(a*b);
 	}
 }
