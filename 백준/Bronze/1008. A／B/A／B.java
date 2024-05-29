dbec560f6719205e613810fef646417d239d9e66
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-29 14:14:52 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
-class Main {
+
+public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in); 
+		// Scanner 클래스의 객체 생성을 먼저 해야함. ( 사용자 입력 )
+		// 참조변수 이름은 자유롭게.
 		
-		int A = in.nextInt();
-		int B = in.nextInt();
-		double C = (double)A/B;
-		System.out.println(C);
+		// Scanner 클래스의 nextInt 메서드 활용하기.
+		
+		int a = in.nextInt(); // nextInt는 정수 입력받기.
+		int b = in.nextInt(); // nextInt는 정수 입력받기.
+		
+		System.out.println((double)a/b); 
+		// a를 double로 형변환하면 double / int여서 b도 double로 자동 형변환.
+		// double / double이니 double의 결과값이 나옴.
+		// float로 형변환해버리면 6~7자리까지만 정밀하기 때문에 안됨.
+		// double은 14~15자리까지의 정밀도가 보장됨.
 	}
 }
