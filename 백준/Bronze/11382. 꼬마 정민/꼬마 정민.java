f6462eee6702b27f6b5d7708f94fc53cfcd5fef5
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-29 16:01:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-class Main {
+
+public class Main {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); 
+		// Scanner 클래스의 객체 생성을 먼저 해야함. ( 사용자 입력 )
+		// 참조변수 이름은 자유롭게.
 		
-		long a, b, c;
-		Scanner in = new Scanner(System.in);
-		
-		a = in.nextLong();
-		b= in.nextLong();
-		c=in.nextLong();
+		long a = in.nextLong();
+		long b = in.nextLong();
+		long c = in.nextLong();
 		
 		System.out.println(a+b+c);
-		in.close();
 	}
 }
