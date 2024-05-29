22fbd4186e8d07a7b96a6e2d315cd8160b8ad008
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-29 16:04:47 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,9 +1,10 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
 	public static void main(String[] args) {
-		
-		System.out.println("\\    /\\");	
-		System.out.println(" )  ( \')");	
-		System.out.println("(  /  )");	
-		System.out.println(" \\(__)|");	
-		}
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
 }
