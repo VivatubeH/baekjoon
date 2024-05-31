3068b32e05ec0e83f44fd7d06ad6035f07c37fe6
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-31 14:34:04 +0900

    [Bronze V] Title: 구구단, Time: 204 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-	
-		int a = in.nextInt();
 		
-		for (int i=1;i<=9;i++) {
-			System.out.println(""+a+" "+"*"+" "+i+" "+"="+" "+a*i);
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
 		}
 	}
 }
