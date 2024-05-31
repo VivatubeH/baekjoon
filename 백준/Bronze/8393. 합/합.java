2d7dca4e600ef9fc2ce3f62bbdba98e46f2914f6
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-05-31 14:40:51 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-	
-		int t = in.nextInt();
 		
-			System.out.println(t*(t+1)/2);
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		
+		System.out.println(n*(n+1)/2);
 	}
 }
