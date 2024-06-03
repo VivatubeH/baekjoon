c0cdc583e3a9659fe75d8bde666193492e77d6b0
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2024-06-03 09:00:52 +0900

    [Bronze IV] Title: 영수증, Time: 220 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 
-class Main{
+public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-	
+		
 		int x = in.nextInt();
 		int n = in.nextInt();
-		int sum = 0;
 		
-		for ( int i =0; i<n; i++) {
+		// x는 영수증에 적힌 총 금액 x, y는 영수증에 적힌 구매한 물건 수
+		
+		int total = 0; // 실제로 계산할 총 금액을 저장할 변수 total
+		
+		for (int i = 1; i <= n; i++) {
 			int a = in.nextInt();
 			int b = in.nextInt();
-			sum += (a*b);
+			total += (a*b); // a*b를 계속 total에 누적해서 계산
 		}
-		System.out.println((x==sum)?"Yes":"No");
+		
+		if(x == total) { // 총 금액과 계산한 금액이 일치하면
+			System.out.println("Yes");
+		}
+		else { // 일치하지 않으면
+			System.out.println("No");
+		}
+		in.close();
 	}
 }
