2dc3621556745fcff20833ec4a788b675592af84
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-03-17 13:51:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1420 ms, Memory: 140036 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,18 +2,21 @@ import com.sun.source.tree.Tree;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // 100만개를 오름차순 정렬...
-        // TreeSet은 중복 자동 제거 + 자동 오름차순 정렬이 지원됨.
-        TreeSet<Integer> integers = new TreeSet<>();
+        // 중복이 주어지지 않는 게 보장된다면 ArrayList가 속도에서 더 효율적이다.
+        ArrayList<Integer> integers = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         for (int i = 1; i <= n; i++) {
             integers.add(Integer.parseInt(br.readLine()));
         }
+        Collections.sort(integers);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int integer : integers) {
             bw.write(integer + "\n");
