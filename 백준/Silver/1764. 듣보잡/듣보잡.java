15d1e85ce23095dc86584809aacaec37fea5aee0
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-04-01 00:06:30 +0900

    [Silver IV] Title: 듣보잡, Time: 352 ms, Memory: 30456 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -3,9 +3,7 @@ import com.sun.source.tree.Tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,8 +15,8 @@ public class Main {
 
         // 문제의 조건을 보면, 듣보잡의 수와 명단을 사전순으로 출력해야 합니다
         // 이 때는 입력부터가 사전 순으로 되는 것이 좋습니다.
-        Set<String> noHeard = new TreeSet<>();
-        Set<String> noSeen = new TreeSet<>();
+        Set<String> noHeard = new HashSet<>();
+        Set<String> noSeen = new HashSet<>();
 
         // 듣도 못한 사람과 보도 못한 사람을 순서대로 입력받습니다.
         for (int i = 0; i < N; i++) {
@@ -29,12 +27,21 @@ public class Main {
         }
 
         // 정답 명단을 저장합니다.
-        Set<String> answer = new TreeSet<>(noHeard);
+        Set<String> answer = new HashSet<>(noHeard);
         answer.retainAll(noSeen);
 
-        System.out.println(answer.size());
+        // 오름 차순 정렬하겠습니다.
+        ArrayList<String> lists = new ArrayList<>(answer);
+        lists.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
 
-        for(String name : answer) {
+        System.out.println(lists.size());
+
+        for(String name : lists) {
             System.out.println(name);
         }
     }
