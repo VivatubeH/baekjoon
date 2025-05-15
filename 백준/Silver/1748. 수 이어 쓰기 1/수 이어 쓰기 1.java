0f2914405e2a35e812fc688052611b508667a83d
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-05-15 14:45:46 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -6,38 +6,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 사용자로부터 N을 입력받습니다.
 
-        // 사용자가 입력한 N을 토대로 자릿수를 구하는 메서드를 생성합니다.
-        int answer = getLengthByNumber(N);
+        // N의 자릿수를 체크합니다.
+        int length = String.valueOf(N).length();
 
-        System.out.print(answer); // 어차피 하나 출력할 거면 print로 출력하겠습니다.
-    }
-
-    // 연산은 단순하게 구현하겠습니다.
-    public static int getLengthByNumber(int num) {
-        int length = String.valueOf(num).length(); // 해당 수의 자릿수를 구합니다.
-
-        // length 자릿수라면 1 x 9 + 2 x 90 + 3 x 900 + 4 x 9000 + 5 x 90000 + 6 x 900000 + 8 x 9000000까지 다 들어가야함
-        // 1자릿수 -> 9까지 : 1 x 9 + 2 x 6 = 15일 때
-        // 2자릿수 -> 99까지 : 1 x 9 + 2 x 90 + 3 x 21 =
-        // 3자릿수 -> 999까지 : 1 x 9 + 2 x 90 + 3 x 900
-        if (length == 1) { // 한 자릿수면
-            return num; // 그대로
-        } else if (length == 2) { // 두 자릿수면
-            return 9 + (num - 9) * 2;
-        } else if (length == 3) { // 세 자릿수면
-            return 9 + 2 * 90 + (num - 99) * 3;
-        } else if (length == 4) { // 네 자릿수면
-            return 9 + 2 * 90 + 3 * 900 + (num - 999) * 4;
-        } else if (length == 5) {
-            return 9 + 2 * 90 + 3 * 900 + 4 * 9000 + (num - 9999) * 5;
-        } else if (length == 6) {
-            return 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + (num - 99999) * 6;
-        } else if (length == 7) {
-            return 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + 6 * 900000 + (num - 999999) * 7;
-        } else if (length == 8) {
-            return 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + 6 * 900000 + 7 * 9000000 + (num - 9999999) * 8;
+        // 그러면 자릿수 - 1까지는 별도 계산없이 누적합니다.
+        // 누적은 i가 1일때 9, i가 2일때 90, i가 3일때 900 이런식이니깐
+        // sum += 9 * Math.pow(10, i - 1);
+        int sum = 0;
+        for (int i = 1; i <= length - 1; i++) {
+            sum += 9 * i * (int)Math.pow(10, i - 1);
         }
 
-        return 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + 6 * 900000 + 7 * 9000000 + 8 * 90000000 + 9;
+        // 마지막으로 자기 자리수에 도달하면 해당 수까지의 같은 자리수를 세서 누적합니다.
+        // 예를 들어 120까지의 세자릿수는 120 - 100 + 1입니다.
+        // 한 자릿수일 때도 성립합니다.
+        int count = N - (int)(Math.pow(10, length - 1)) + 1; // 같은 자리의 수는
+        sum += (count * length); // length 자리수 x count만큼을 추가로 누적해주면 됩니다.
+
+        System.out.print(sum);
     }
 }
