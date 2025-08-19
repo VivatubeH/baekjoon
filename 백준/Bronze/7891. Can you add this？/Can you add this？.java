6c4d41ccce982cb2159f3c8d80b38f4e817f2168
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-08-19 16:31:00 +0900

    [Bronze V] Title: Can you add this?, Time: 104 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testcase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            System.out.println(x + y);
+        }
+    }
+}
