0b9ab4d9ac763d58f1af2972d83c186b1639619f
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-08-19 16:59:38 +0900

    [Bronze IV] Title: 모음의 개수, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
