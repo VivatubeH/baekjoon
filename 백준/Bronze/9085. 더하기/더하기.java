664554d079d9476f2316017d7bc7ee78e660bbeb
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-08-21 10:47:37 +0900

    [Bronze III] Title: 더하기, Time: 96 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testcase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            System.out.println(sum);
+        }
+    }
+}
