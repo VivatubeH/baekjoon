2ccc0f0e55aff7a3d30e481972f2797a01c89486
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-09-10 10:57:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 684 ms, Memory: 48268 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,56 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Member> members = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
-            int age = Integer.parseInt(tokenizer.nextToken());
-            String name = tokenizer.nextToken();
-            members.add(new Member(age, name, i));
-        }
-        Collections.sort(members, new Comparator<Member>() {
-            @Override
-            public int compare(Member o1, Member o2) {
-                // 나이가 같으면 먼저 들어온 게 앞으로 가고
-                // 나이가 다르면 나이순으로 정렬이 됩니다.
-                return o1.getAge() == o2.getAge() ? o1.getOrder() - o2.getOrder() : Integer.compare(o1.getAge(), o2.getAge());
-            }
-        });
-        for (int i = 0; i < N; i++) {
-            System.out.println(members.get(i).getAge() + " "  + members.get(i).getName());
-        }
-    }
+	public static List<Member> list = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		service();
+		output();
+	}
+	
+	public static void input() throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int N = Integer.parseInt(br.readLine());
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int age = Integer.parseInt(st.nextToken());
+				String name = st.nextToken();
+				
+				Member member = new Member(i, age, name);
+				list.add(member);
+			}
+		}
+	}
+	
+	public static void service() throws IOException {
+		Collections.sort(list);
+	}
+	
+	public static void output() throws IOException {
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+			for (Member member : list) {
+				bw.write(member.getAge() + " " + member.getName());
+				bw.write("\n");
+			}
+			bw.flush();
+		}
+	}
 }
 
-class Member {
-    private int order;
-    private int age;
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    Member(int age, String name, int order) {
-        this.age = age;
-        this.name = name;
-        this.order = order;
-    }
+class Member implements Comparable<Member> {
+	private int order;
+	private int age;
+	private String name;
+	
+	public Member(int order, int age, String name) {
+		this.order = order;
+		this.age = age;
+		this.name = name;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Member member) {
+		if (this.age != member.getAge()) {
+			return this.age - member.getAge();
+		} 
+		return this.order - member.getOrder();
+	}
 }
