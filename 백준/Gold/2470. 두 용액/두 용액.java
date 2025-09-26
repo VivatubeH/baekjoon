f90e397d36af40f9f2879a79fc0513ea2b7f1fb4
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-09-26 16:51:35 +0900

    [Gold V] Title: 두 용액, Time: 352 ms, Memory: 28856 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,73 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int[] array;
+    public static int answer1;
+    public static int answer2;
+
     public static void main(String[] args) throws IOException {
-        // N을 입력받는다.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        // list 생성 후 N개의 특성 값 저장
-        ArrayList<Integer> list = new ArrayList<>();
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(token.nextToken()));
-        }
-        // 오름차순 정렬
-        Collections.sort(list);
-        // binarySearch 메서드를 통해 가장 작은 두 특성값 가져오기
-        int[] minAndMax = twoPointers(list);
-        System.out.print(minAndMax[0] + " " + minAndMax[1]);
+        input();
+        service();
+        output();
     }
 
-    public static int[] twoPointers(ArrayList<Integer> array) {
-        int leftIndex = 0; // 왼쪽 포인터 위치를 저장할 변수
-        int rightIndex = array.size() - 1; // 우측 포인터 위치를 저장할 변수
-
-        int leftValue = 0; // 왼쪽 포인터에 담길 값
-        int rightValue = 0; // 우측 포인터에 담길 값
-
-        int min = Integer.MAX_VALUE; // 현재까지의 최솟값을 저장할 변수
-        int sum = 0; // 지금 조합의 합을 구할 변수
-
-        // 최종적으로 구한 용액의 특성값 2개를 구합니다.
-        int targetLeft = array.get(leftIndex);
-        int targetRight = array.get(rightIndex);
-        min = Math.abs(targetLeft + targetRight);
-
-        // 두 포인터가 교차될 때까지 수행합니다.
-        while(leftIndex < rightIndex) {
-            // 왼쪽 포인터가 가리키는 값과 오른쪽 포인터가 가리키는 값을
-            // 각각 Value 변수에 저장합니다.
-            leftValue = array.get(leftIndex);
-            rightValue = array.get(rightIndex);
-
-            // 조건 검사
-
-            // 그 두개의 값을 더해서 sum을 구합니다.
-            sum = leftValue + rightValue;
-
-            // 이 sum이 0과 떨어진 거리가 min보다 작으면 갱신합니다.
-            if (Math.abs(sum) < min) {
-                targetLeft = leftValue;
-                targetRight = rightValue;
-                min = Math.abs(sum);
-            }
-
-            // 이동
-            // 합이 0보다 작으면 왼쪽 인덱스를 키워주고,
-            // 합이 0보다 크다면 오른쪽 인덱스를 줄여줍니다.
-            // 합이 0이라면 해당 두 수를 담고 종료합니다.
-            if (sum < 0) leftIndex++;
-            else if (sum > 0) rightIndex--;
-            else {
-                targetLeft = leftValue;
-                targetRight = rightValue;
-                break;
+    public static void input() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            array = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                array[i] = Integer.parseInt(st.nextToken());
             }
         }
+    }
 
-        int[] answer = new int[2];
-        answer[0] = targetLeft;
-        answer[1] = targetRight;
-        return answer;
+    public static void service() {
+        Arrays.sort(array);
+        binarySearch();
+    }
+
+    public static void binarySearch() {
+        // 갱신를 위해서 최솟값을 최대한 크게 설정한다.
+        int minSum = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            int current = array[i];
+
+            // current와 더했을 때의 절댓값이 최소인 수를 이분탐색
+            int lower = 0;
+            int upper = array.length - 1;
+
+            while (lower <= upper) {
+                int mid = (lower + upper) / 2;
+
+                int sum = current + array[mid];
+                int abs = Math.abs(sum);
+
+                // 합의 절댓값이 지금까지 최소합보다 작거나 같으면
+                if (abs <= minSum && (mid != i)) {
+                    minSum = abs;
+                    answer1 = current;
+                    answer2 = array[mid];
+                }
+
+                // 합의 절댓값이 최소합보다 작든 크든 탐색은 지속
+                if (sum > 0) { // 더 작은데서 찾기
+                    upper = mid - 1;
+                } else { // 더 큰데서 찾기
+                    lower = mid + 1;
+                }
+            }
+        }
+    }
+
+    public static void output() {
+        System.out.print(Math.min(answer1, answer2) + " " + Math.max(answer1, answer2));
     }
 }
