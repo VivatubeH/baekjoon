21b29e9cdb4062038e6d812b0fb83511ad2b18bc
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-10-17 09:56:15 +0900

    [Silver IV] Title: 큐, Time: 168 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -2,55 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static ArrayList<Integer> results = new ArrayList<>();
-	static Deque<Integer> deque = new ArrayDeque<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < N; i++) {
-			String instruction = br.readLine();
-			execute(instruction);
-		}
-		for (Integer num : results) {
-			sb.append(num + "\n");
-		}
-		System.out.print(sb);
-	}
-	
-	public static void execute(String instruction) {
-		if (instruction.contains("push")) {
-			String[] token = instruction.split(" ");
-			int num = Integer.parseInt(token[1]);
-			deque.offerLast(num);
-		} else if (instruction.equals("pop")) {
-			if (deque.size() == 0) {
-				results.add(-1);
-			} else {
-				results.add(deque.pollFirst());
-			}
-		} else if (instruction.equals("size")) {
-			results.add(deque.size());
-		} else if (instruction.equals("empty")) {
-			if (deque.isEmpty()) {
-				results.add(1);
-			} else {
-				results.add(0);
-			}
-		} else if (instruction.equals("front")) {
-			if (deque.isEmpty()) {
-				results.add(-1);
-			} else {
-				results.add(deque.peekFirst());	
-			}
-		} else if (instruction.equals("back")) {
-			if (deque.isEmpty()) {
-				results.add(-1);
-			} else {
-				results.add(deque.peekLast());
-			}
-		}
-	}
+    public static Queue<Integer> queue = new ArrayDeque<>();
+    public static StringBuilder answer = new StringBuilder();
+    public static int backNumber = 0;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.print(answer);
+    }
+
+    public static void input() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            for (int i = 0; i < N; i++) {
+                String instruction = br.readLine();
+                execute(instruction);
+            }
+        }
+    }
+
+    public static void execute(String instruction) {
+        if (instruction.startsWith("push")) {
+            StringTokenizer st = new StringTokenizer(instruction);
+            st.nextToken();
+            int num = Integer.parseInt(st.nextToken());
+            backNumber = num;
+            queue.offer(num);
+        } else if (instruction.equals("pop")) {
+            if (!queue.isEmpty()) {
+                answer.append(queue.poll());
+            } else {
+                answer.append(-1);
+            }
+            answer.append("\n");
+        } else if (instruction.equals("size")) {
+            answer.append(queue.size()).append("\n");
+        } else if (instruction.equals("empty")) {
+            if (queue.isEmpty()) answer.append(1);
+            else answer.append(0);
+            answer.append("\n");
+        } else if (instruction.equals("front")) {
+            if (queue.isEmpty()) answer.append(-1);
+            else answer.append(queue.peek());
+            answer.append("\n");
+        } else if (instruction.equals("back")) {
+            if (queue.isEmpty()) answer.append(-1);
+            else answer.append(backNumber);
+            answer.append("\n");
+        }
+    }
 }
