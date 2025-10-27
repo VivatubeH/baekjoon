ae071457cc237709ab864f34e380f12c630030aa
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-10-28 08:58:36 +0900

    [Silver III] Title: N과 M (5), Time: 276 ms, Memory: 29144 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int M;
-    public static int[] candidate;
     public static boolean[] isSelected;
+    public static int[] candidate;
     public static int[] selected;
     public static StringBuilder answer = new StringBuilder();
+    public static int N;
+    public static int M;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -15,25 +15,8 @@ public class Main {
         System.out.print(answer);
     }
 
-    public static void input() throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer firstLine = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(firstLine.nextToken());
-            M = Integer.parseInt(firstLine.nextToken());
-            candidate = new int[N];
-            isSelected = new boolean[N];
-            selected = new int[M];
-
-            StringTokenizer secondLine = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                candidate[i] = Integer.parseInt(secondLine.nextToken());
-            }
-            Arrays.sort(candidate);
-        }
-    }
-
     public static void permutation(int depth) {
-        // M개를 다 뽑았으면
+        // 종료 조건 ( 종료 시 수행할 작업 + return으로 종료 )
         if (depth == M) {
             for (int i = 0; i < M; i++) {
                 answer.append(selected[i]).append(" ");
@@ -41,13 +24,31 @@ public class Main {
             answer.append("\n");
             return;
         }
+        // 중복은 허용하지 않으므로 해당 원소를 사용했는지 여부 체크.
+        // 1, 7 / 7, 1 조합이 둘 다 가능하므로 i = 0부터 다시 탐색
         for (int i = 0; i < N; i++) {
-            if (!isSelected[i]) {
-                isSelected[i] = true;
-                selected[depth] = candidate[i];
-                permutation(depth + 1);
-                isSelected[i] = false;
+            if (!isSelected[i]) { // 안 뽑은 원소에 대해서만
+                isSelected[i] = true; // 뽑고
+                selected[depth] = candidate[i]; // 원소를 저장한 다음
+                permutation(depth + 1); // 다음 원소를 뽑으러 감
+                isSelected[i] = false; // 다음 케이스를 체크하기 위해 복구
             }
+        }
+    }
+
+    public static void input() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            candidate = new int[N];
+            isSelected = new boolean[N];
+            selected = new int[M];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                candidate[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(candidate);
         }
     }
 }
