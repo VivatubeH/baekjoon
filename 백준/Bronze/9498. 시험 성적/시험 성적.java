9d1d48448f1afb15e8af7955edf672dba48054ed
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-04 14:54:22 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int point = scanner.nextInt();
-		if ( point >= 90 ) System.out.println("A");
-		else if ( point >= 80 ) System.out.println("B");
-		else if ( point >= 70 ) System.out.println("C");
-		else if ( point >= 60 ) System.out.println("D");
-		else System.out.println("F");
-		scanner.close();
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+       Scanner scanner = new Scanner(System.in);
+       int score = scanner.nextInt();
+
+       if (score >= 90) {
+           System.out.print("A");
+       } else if (score >= 80) {
+           System.out.print("B");
+       } else if (score >= 70) {
+           System.out.print("C");
+       } else if (score >= 60) {
+           System.out.print("D");
+       } else {
+           System.out.print("F");
+       }
+    }
 }
