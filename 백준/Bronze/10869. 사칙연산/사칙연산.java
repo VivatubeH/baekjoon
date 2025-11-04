d0a87e55f1f7d401dee50dc87fb1d28e87470bf2
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-04 14:46:32 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,20 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in); 
-		// Scanner 클래스의 객체 생성을 먼저 해야함. ( 사용자 입력 )
-		// 참조변수 이름은 자유롭게.
-		
-		// Scanner 클래스의 nextInt 메서드 활용하기.
-		
-		int a = in.nextInt(); // nextInt는 정수 입력받기.
-		int b = in.nextInt(); // nextInt는 정수 입력받기.
-		
-		System.out.println(a+b);
-		System.out.println(a-b);
-		System.out.println(a*b);
-		System.out.println(a/b);
-		System.out.println(a%b);
-	}
+    public static void main(String[] args) throws IOException {
+       Scanner scanner = new Scanner(System.in);
+       int A = scanner.nextInt();
+       int B = scanner.nextInt();
+       System.out.println(A + B);
+       System.out.println(A - B);
+       System.out.println(A * B);
+       System.out.println(A / B);
+       System.out.println(A % B);
+    }
 }
