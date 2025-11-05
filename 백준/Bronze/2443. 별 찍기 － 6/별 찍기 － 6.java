32d907541ba94cf2a1cb551ab91ff848138674a8
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-05 13:08:19 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 316 ms, Memory: 18916 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-
-        // 규칙 찾기
-        // i번째 줄의 별의 개수 -> 2*N - (2*i + 1)개
-        // i번째 줄의 공백의 개수 -> i개
-        for (int i = 0; i < N; i++) { // N개의 줄에 대해서
-            for (int j = 0; j < i; j++) { // i개의 공백 찍기
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k < 2*(N-i) - 1; k++) { // 2*N - (2*i + 1)개의 별 찍기
+            for (int j = 1; j <= 2 * N - (2 * i - 1); j++) {
                 System.out.print("*");
             }
-            System.out.println(); // 공백과 별을 찍은 후엔 줄바꿈
+            System.out.println();
         }
     }
 }
