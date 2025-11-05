093480c6fc25b301c9460f05b40e17868def7bf9
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-05 11:06:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 752 ms, Memory: 236184 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = scanner.nextInt();
+        int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             bw.write(A + B + "\n");
         }
         bw.flush();
