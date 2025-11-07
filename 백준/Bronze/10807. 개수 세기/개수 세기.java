dc481ee5faed791f6bbd14bb8699631634970e7c
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-07 12:51:40 +0900

    [Bronze V] Title: 개수 세기, Time: 184 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,20 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-		int n = in.nextInt();
-		int[] t = new int[n];
-		
-		for(int i=0;i<n;i++) {
-			t[i] = in.nextInt();
-		}
-		int k = in.nextInt();
-		int num = 0;
-		for (int i =0; i<n;i++) {
-			if ( t[i] == k ) num++;
-		}
-		System.out.println(num);
-	}
+        int[] positive = new int[101];
+        int zero = 0;
+        int[] negative = new int[101];
+
+        for (int i = 0; i < n; i++) {
+            int current = scanner.nextInt();
+
+            if (current > 0) {
+                positive[current]++;
+            } else if (current < 0) {
+                negative[current * -1]++;
+            } else {
+                zero++;
+            }
+        }
+
+        int v = scanner.nextInt();
+
+        if (v > 0) System.out.print(positive[v]);
+        else if (v < 0) System.out.print(negative[v * -1]);
+        else System.out.print(zero);
+    }
 }
