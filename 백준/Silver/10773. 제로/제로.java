adfe28d508e59a9bd8a40c4de6c178217f3104c0
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2025-11-21 14:27:46 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 23364 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,25 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static Deque<Integer> stack = new ArrayDeque<>();
+    public static int sum = 0;
+
     public static void main(String[] args) throws IOException {
-        // 추상 인터페이스 deque을 구현한 ArrayDeque을 stack처럼 사용하기.
-        Deque<Integer> deque = new ArrayDeque<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
-        for (int i = 0; i < K; i++) { // K번 정수를 입력받습니다.
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) { // 0이면 마지막 원소제거 ( 스택처럼 위(뒤)에서 부터 제거 )
-                deque.removeLast(); // 마지막 원소를 제거합니다.
-            } else {
-                deque.addLast(num); // 마지막에 값을 추가합니다. ( 스택처럼 뒤에 값 추가 )
+        input();
+        System.out.print(sum);
+    }
+
+    public static void input() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int K = Integer.parseInt(br.readLine());
+            for (int i = 0; i < K; i++) {
+                int num = Integer.parseInt(br.readLine());
+                execute(num);
             }
         }
-        // 최종적으로 적어낸 수의 합을 구합니다.
-        int sum = 0;
-        for (Integer integer : deque) {
-            sum += integer;
+    }
+
+    public static void execute(int num) {
+        if (num == 0) {
+            int minus = stack.pop();
+            sum -= minus;
+        } else {
+            stack.push(num);
+            sum += num;
         }
-        System.out.println(sum);
     }
 }
