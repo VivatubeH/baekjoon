40f741cdda76cf40021162ce33d5bd13f22ce69f
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-01-13 02:43:24 +0900

    [Silver III] Title: 개미, Time: 92 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -2,30 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer firstToken = new StringTokenizer(br.readLine());
-        int w = Integer.parseInt(firstToken.nextToken());
-        int h = Integer.parseInt(firstToken.nextToken());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
 
-        StringTokenizer secondToken = new StringTokenizer(br.readLine());
-        int p = Integer.parseInt(secondToken.nextToken());
-        int q = Integer.parseInt(secondToken.nextToken());
+        int p = scanner.nextInt();
+        int q = scanner.nextInt();
 
-        int t = Integer.parseInt(br.readLine());
+        int t = scanner.nextInt();
 
-        int moveTimeX = t % (2*w);
-        int moveTimeY = t % (2*h);
+        int xPosition = 0;
+        int yPosition = 0;
 
-        int x = (p + moveTimeX) % (2 * w);
-        int y = (q + moveTimeY) % (2 * h);
+        // x와 y가 실제로 이동하는 시간
+        int realtimeX = t % (2 * w);
+        int realtimeY = t % (2 * h);
 
-        if (x > w && x < (2 * w)) {
-            x = w - (x % w);
+        // 개미가 0에서부터 움직였다고 가정하면
+        // p초후에 p위치로 오기 때문에
+        // 0부터 p + realtimeX까지 이동한다고 보면 된다.
+        int totalmoveX = p + realtimeX;
+        int totalmoveY = q + realtimeY;
+
+        // 이 이동거리를 2 * w, 2 * h로 나눈 나머지를 각각 구한다
+        int remainderX = totalmoveX % (2 * w);
+        int remainderY = totalmoveY % (2 * h);
+
+        // remainder 모든 왕복이 종료된 후, 개미는 0에서
+        // remainder 만큼 떨어진 곳에 위치한다.
+        // 만약 remainder이 w보다 작거나 같다면 우측으로 이동중이니
+        if (remainderX <= w) {
+            xPosition = remainderX; // 최종 좌표는 그대로
+        } else { // w보다 크다면 우측에서 반대로 이동중이니
+            xPosition = w - (remainderX - w);
         }
-        if (y > h && y < (2 * h)) {
-            y = h - (y % h);
+
+        if (remainderY <= h) {
+            yPosition = remainderY; // 최종 좌표는 그대로
+        } else {
+            yPosition = h - (remainderY - h);
         }
-        System.out.print(x + " " + y);
+
+        System.out.print(xPosition + " " + yPosition);
     }
 }
