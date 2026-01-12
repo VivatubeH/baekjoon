4cde009007e87a52a11e84d79b4c9ec14818da96
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-01-13 02:59:33 +0900

    [Silver III] Title: 개미, Time: 124 ms, Memory: 16192 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -2,48 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int w = scanner.nextInt();
-        int h = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int p = scanner.nextInt();
-        int q = scanner.nextInt();
+        StringTokenizer firstTokenizer = new StringTokenizer(br.readLine());
+        StringTokenizer secondTokenizer = new StringTokenizer(br.readLine());
+        // 격자의 w, h가 주어짐
+        int w = Integer.parseInt(firstTokenizer.nextToken());
+        int h = Integer.parseInt(firstTokenizer.nextToken());
 
-        int t = scanner.nextInt();
+        // 초기 좌표값 p, q가 주어짐
+        int p = Integer.parseInt(secondTokenizer.nextToken());
+        int q = Integer.parseInt(secondTokenizer.nextToken());
 
-        int xPosition = 0;
-        int yPosition = 0;
+        // 개미가 움직일 시간 t가 주어짐
+        int t = Integer.parseInt(br.readLine());
 
-        // x와 y가 실제로 이동하는 시간
-        int realtimeX = t % (2 * w);
-        int realtimeY = t % (2 * h);
+        // 주기는 2w, 2h임. 이렇게 하면 주기를 제외한 실제 이동시간만 남음.
+        int realMoveX = t % (2 * w); // 0 <= realMoveX < 2w
+        int realMoveY = t % (2 * h); // 0 <= realMoveY < 2h
 
-        // 개미가 0에서부터 움직였다고 가정하면
-        // p초후에 p위치로 오기 때문에
-        // 0부터 p + realtimeX까지 이동한다고 보면 된다.
-        int totalmoveX = p + realtimeX;
-        int totalmoveY = q + realtimeY;
+        // 현재 X, Y는 주기 ( 0 ~ 2 * w ) 사이에 위치하게 된다.
+        int currentX = (p + realMoveX) % (2 * w);
+        int currentY = (q + realMoveY) % (2 * h);
 
-        // 이 이동거리를 2 * w, 2 * h로 나눈 나머지를 각각 구한다
-        int remainderX = totalmoveX % (2 * w);
-        int remainderY = totalmoveY % (2 * h);
-
-        // remainder 모든 왕복이 종료된 후, 개미는 0에서
-        // remainder 만큼 떨어진 곳에 위치한다.
-        // 만약 remainder이 w보다 작거나 같다면 우측으로 이동중이니
-        if (remainderX <= w) {
-            xPosition = remainderX; // 최종 좌표는 그대로
-        } else { // w보다 크다면 우측에서 반대로 이동중이니
-            xPosition = w - (remainderX - w);
+        // [0, w], [0, h]에 있으면 그대로 좌표
+        // 단, (w, 2w), (h, 2h)에 있으면 반대방향 진행이니 빼줘야 한다.
+        if (currentX > w) {
+            currentX = w - (currentX % w);
+        }
+        if (currentY > h) {
+            currentY = h - (currentY % h);
         }
 
-        if (remainderY <= h) {
-            yPosition = remainderY; // 최종 좌표는 그대로
-        } else {
-            yPosition = h - (remainderY - h);
-        }
-
-        System.out.print(xPosition + " " + yPosition);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(currentX + " " + currentY);
+        bw.flush();
     }
 }
