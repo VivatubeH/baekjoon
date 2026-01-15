5c991b0565eb7e04c3176a9b1c100183eb3744b1
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-01-15 14:50:20 +0900

    [Bronze I] Title: 성 지키기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -8,31 +8,44 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        boolean[] hasRowGuard = new boolean[N];
-        boolean[] hasColGuard = new boolean[M];
+        // 행, 열별로 경비원이 있는지 체크
+        // 1부터 시작하는 인덱스를 갖기 위해 1 크게 만듬.
+        // boolean의 기본값이 false니 별도 초기화 x
+        boolean[] rowExist = new boolean[N + 1];
+        boolean[] colExist = new boolean[M + 1];
 
-        for (int i = 0; i < N; i++) {
+        // 시간 복잡도 O(NxM)
+        for (int row = 1; row <= N; row++) {
             String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                if (line.charAt(j) == 'X') {
-                    hasRowGuard[i] = true;
-                    hasColGuard[j] = true;
+            for (int col = 1; col <= M; col++) {
+                // col이 1부터니 charAt은 0부터로 보정
+                char ch = line.charAt(col - 1);
+                // 경비원이 있는 경우에
+                // row, col에는 경비원이 존재한다.
+                if (ch == 'X') {
+                    rowExist[row] = true;
+                    colExist[col] = true;
                 }
             }
         }
-        int noGuardRow = 0;
-        int noGuardCol = 0;
 
-        for (int i = 0; i < N; i++) {
-            if (!hasRowGuard[i]) {
-                noGuardRow++;
+        // row, col 별로 빈 경비원의 수를 기록하기 위한 변수
+        int rowCount = 0;
+        int colCount = 0;
+
+        // rowExist, colExist중 false인 칸의 개수를 센다.
+        for (int i = 1; i <= N; i++) {
+            if (!rowExist[i]) {
+                rowCount++;
             }
         }
-        for (int i = 0; i < M; i++) {
-            if (!hasColGuard[i]) {
-                noGuardCol++;
+        for (int j = 1; j <= M; j++) {
+            if (!colExist[j]) {
+                colCount++;
             }
         }
-        System.out.print(Math.max(noGuardRow, noGuardCol));
+
+        // 둘 중 더 많은 수가 추가해야 하는 최소 경비원 수
+        System.out.print(Math.max(rowCount, colCount));
     }
 }
