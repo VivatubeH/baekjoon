9939c970e607314bdfb24b863f8d09701b78314d
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-01-21 16:12:51 +0900

    [Silver II] Title: 좌표 압축, Time: 1520 ms, Memory: 280008 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -4,44 +4,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-
-        // 먼저 입력받은 숫자를 저장합니다.
-        // O(n) -> 최대 100만회의 연산
-        int[] numbers = new int[N];
-        int originalIndex = 0;
-        while (tokenizer.hasMoreTokens()) {
-            numbers[originalIndex++] = Integer.parseInt(tokenizer.nextToken());
-        }
-
-        // 기존 인덱스는 유지해야 하기 때문에, 배열을 복사해서 정렬합니다.
-        // O(n log n) -> 최대 100만 x 6, 600만회의 연산
-        int[] sortedArray = Arrays.copyOf(numbers, N);
-        Arrays.sort(sortedArray);
-
-        // HashMap에 중복을 제거해서 담습니다.
-        // HashMap에 값 저장 -> O(1) x n회 -> 최대 100만회의 연산
-        HashMap<Integer, Integer> answer = new HashMap<>();
-        int answerIndex = 0; // 정답이 될 인덱스를 담습니다.
-        for (int i = 0; i < sortedArray.length; i++) {
-            if (!(answer.containsKey(sortedArray[i]))) { // 해당 숫자를 키로 가지고 있지 않아야 값을 대입합니다. ( 중복 제거 )
-                answer.put(sortedArray[i], answerIndex++); // 이렇게 해주면 ArrayList에 있는 정렬된 순서대로 값을 hashMap에 담습니다.
-            }
-        }
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        // 꺼낼 때는 원래 배열의 순서대로 map의 value ( 인덱스 ) 를 꺼내면 됩니다.
-        // 단순히 꺼냄 최대 100만회의 연산
-        // 1회째 대량 출력 때, 시간 초과가 났으니 BufferedWriter로 변경
-        for (int num : numbers) {
-            bw.write(answer.get(num) + " ");
+        int N = Integer.parseInt(br.readLine()); // N개의 좌표의 개수 N이 주어진다.
+        StringTokenizer st = new StringTokenizer(br.readLine()); // N개의 정수가 공백을 기준으로 주어진다.
+        List<Integer> list = new ArrayList<>(); // 입력된 N개의 수를 정렬하기 위한 리스트, 이 리스트는 유지 후 마지막에 출력을 위해 씀.
+        for (int i = 0; i < N; i++) {
+            list.add(i, Integer.parseInt(st.nextToken())); // i번 인덱스에 해당 정수 입력, N개의 정수를 입력한다.
         }
-        bw.flush();
-
-        br.close();
-        bw.close();
+        // 중복을 제거하기 위해 Set에 옮겨담는다.
+        Set<Integer> set = new HashSet<>(list);
+        // 이 Set을 다시 정렬을 위한 List에 옮겨담는다.
+        List<Integer> sortedList = new ArrayList<>(set);
+        Collections.sort(sortedList); // 오름차순 정렬한다.
+        Map<Integer, Integer> map = new HashMap<>(); // 입력받은 수와 인덱스를 매핑하기 위한 HashMap
+        // sortedList의 개수는 N개가 아닐 수 있으므로 sortedList.size()로 인덱스를 재차 체크해야한다.
+        for (int i = 0; i < sortedList.size(); i++) { // 정렬된 리스트의 인덱스를 기반해서 Map에 매핑한다.
+            map.put(sortedList.get(i), i); // 숫자와 인덱스를 매핑한다.
+        }
+        // 입력받았던 수를 공백을 넣어서 출력한다. 해당 수가 Key로 들어가 있는 Map의 Value가 바로 해당 수보다 작은 Xj의 개수이다.
+        for (int i = 0; i < N; i++) {
+            Integer integer = list.get(i); // List의 i번째 수를 가져온다.
+            bw.write(map.get(integer) + " "); // map에 포함된 해당 수의 인덱스를 공백을 넣어서 버퍼에 기록한다.
+        }
+        bw.flush(); // 화면에 출력한다.
     }
 }
