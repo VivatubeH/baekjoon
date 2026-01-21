aea3e5b55ffa314f81fb733586b07e82cdaea474
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-01-21 15:05:38 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 532 ms, Memory: 46428 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,68 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        HashSet<AttendanceLog> lists = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-            String name = tokenizer.nextToken();
-            String attendance = tokenizer.nextToken();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 출입 기록의 수 N이 주어짐
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>(); // 출퇴근 기록을 담을 Set
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken(); // 로그에 나오는 사원명
+            String result = st.nextToken(); // 출입 기록
 
-            // 사용자가 출퇴근을 하면 출퇴근 기록을 AttendanceLog에 추가합니다.
-            // 단 기존에 없으면 추가, 있으면 삭제해야 합니다.
-            AttendanceLog log = new AttendanceLog(name, attendance);
-            if (lists.contains(log)) { // contains는 이름을 보고 판단합니다.
-                lists.remove(log);
-            } else { // 없으면 추가합니다.
-                lists.add(log);
+            if (result.equals("enter")) { // 출근시에는
+               set.add(name); // add는 있을 시 추가 후 true 반환, 없을 시 추가 없이 false 반환
+            } else if (result.equals("leave")) { // 퇴근시에는
+               set.remove(name); // remove는 있을 시 제거 후 true 반환, 없을 시 제거 없이 false 반환
             }
         }
-        ArrayList<AttendanceLog> arraylist = new ArrayList<>(lists);
-        Collections.sort(arraylist, new Comparator<AttendanceLog>() {
-            @Override
-            public int compare(AttendanceLog o1, AttendanceLog o2) {
-                return o2.getName().compareTo(o1.getName());
-            }
-        });
-        for (int i = 0; i < arraylist.size(); i++) {
-            System.out.println(arraylist.get(i).getName());
+        // 로그를 Set에 기록한 뒤 사전순의 역순으로 정렬한다.
+        // * Set은 순서가 없는 자료구조인데 sort 메서드는 인덱스(순서)가 존재하는 List 인터페이스의 구현 객체만 인자로 받는다.
+        List<String> list = new ArrayList<>(set);
+        list.sort((s1, s2) -> s2.compareTo(s1)); // List를 사전순의 역순으로 정렬
+        for (String name : list) { // 역순으로 정렬된 list를 순회하면서
+            bw.write(name); // 이름을 출력하고
+            bw.newLine(); // 줄바꿈을 출력하고
         }
-    }
-}
-
-class AttendanceLog {
-    private String name;
-    private String attendance;
-
-    public AttendanceLog(String name, String attendance) {
-        this.name = name;
-        this.attendance = attendance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        // 주소값이 같으면 같은 객체입니다.
-        if (this == obj) return true;
-        // 형변환 할 수 없으면 다른 객체입니다.
-        if (!(obj instanceof AttendanceLog)) {
-            return false;
-        }
-        // 형변환이 가능하면 형변환해서 비교합니다.
-        AttendanceLog log = (AttendanceLog) obj;
-        return log.name.equals(this.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
+        bw.flush(); // 버퍼의 내용을 flush 한다.
     }
 }
