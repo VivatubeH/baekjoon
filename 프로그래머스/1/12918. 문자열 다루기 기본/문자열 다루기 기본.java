c2e2d94c93b41521e8458c878f3624d9980b8bfb
VivatubeH <149135917+VivatubeH@users.noreply.github.com>
2026-04-27 14:27:44 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        // 길이가 4나 6이 아니면 false 반환
+        if (!(s.length() == 4 || s.length() == 6)) {
+            return false;
+        }
+        // 숫자가 아닌 게 존재하면 false 반환
+        for (int i = 0; i < s.length(); i++) {
+            if (!((s.charAt(i) >= '0') && (s.charAt(i) <= '9'))) {
+                return false;
+            }
+        }
+        // 둘 다 해당 안되면 true
+        return true;
+    }
+}
